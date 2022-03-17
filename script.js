b5b043da71dfb68f8cd5bb4513d73744404b056c
Daniel Filipe <anielilipe90@gmail.com>
2022-03-17 13:59:06 -0300

    Adiciona barra de progresso à aba de tecnologias

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-let audio = document.getElementById("heylisten");
+const audio = document.getElementById("heylisten");
 
 function mouseOver() {
     audio.play();
 };
 
-let itens = document.querySelectorAll('.tecnology-ul li');
+const itens = document.querySelectorAll('.tecnology-ul li');
 let selectedindex = 0;
+
+const progressBarContent = document.querySelector('.progress-bar-content');
+
+function progressBar() {
+    const barWidth = 120/itens.length;
+    const completeBar = barWidth * (selectedindex + 1);
+
+    console.log(completeBar);
+
+    progressBarContent.style.width = `${completeBar}px`;
+}
 
 function leftButton() {
     if (selectedindex === 0) {
         itens[selectedindex].className = "tecnology-item";
         selectedindex = itens.length - 1;
         itens[selectedindex].className = "tecnology-item-selected";
+        progressBar();
         return;
     }
 
     itens[selectedindex].className = "tecnology-item";
     selectedindex-=1;
     itens[selectedindex].className = "tecnology-item-selected";
+    progressBar();
 }
 
 function rigthButton() {
@@ -25,10 +38,12 @@ function rigthButton() {
         itens[selectedindex].className = "tecnology-item";
         selectedindex = 0;
         itens[0].className = "tecnology-item-selected";
+        progressBar();
         return;
     }
 
     itens[selectedindex].className = "tecnology-item";
     selectedindex+=1;
     itens[selectedindex].className = "tecnology-item-selected";
+    progressBar();
 }
